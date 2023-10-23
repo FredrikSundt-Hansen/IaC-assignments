31e37abb8a6bcd9728bfd69d7629bab8162ca80e
fredsu <fredsu@stud.ntnu.no>
2023-10-23 19:09:43 +0200

    readd weekly assignments

---
+++ weekly_assignments/week_40/website/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "fredsu-rg-backend"
+    storage_account_name = "sabetfsdfzp45oee3"
+    container_name       = "tfstate"
+    key                  = "week-40.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}

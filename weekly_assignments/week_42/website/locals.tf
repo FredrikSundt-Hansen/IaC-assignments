535e769ccf6857add647e0f49f6baeaec448c7a6
fredsu <fredsu@stud.ntnu.no>
2023-11-02 13:24:28 +0100

    fix locals.tf

--- weekly_assignments/week_42/website/locals.tf
+++ weekly_assignments/week_42/website/locals.tf
@@ -7,7 +7,7 @@ locals {
     Environment  = terraform.workspace
   }
 
-  rg_website_name = terraform.workspace == "default" ? var.rg_website_name : var.rg_website_name-local.workspace_suffix
+  rg_website_name = terraform.workspace == "default" ? var.rg_website_name : "${var.rg_website_name}-${local.workspace_suffix}"
   sa_website_name = terraform.workspace == "default" ? var.sa_website_name : "${var.sa_website_name}${local.workspace_suffix}"
   web_suffix      = "<h2>${terraform.workspace}</h2>"
 }

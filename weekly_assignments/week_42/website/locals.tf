253dc4f45f06388098bb0431788a676e73ef57aa
fredsu <fredsu@stud.ntnu.no>
2023-11-02 13:11:16 +0100

    add changes after tflint and tfsec

--- weekly_assignments/week_42/website/locals.tf
+++ weekly_assignments/week_42/website/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+  workspace_suffix = terraform.workspace == "default" ? "" : terraform.workspace
 
   company_tags = {
     Company      = var.company_name
@@ -7,7 +7,7 @@ locals {
     Environment  = terraform.workspace
   }
 
-  rg_website_name = terraform.workspace == "default" ? "${var.rg_website_name}" : "${var.rg_website_name}-${local.workspace_suffix}"
-  sa_website_name = terraform.workspace == "default" ? "${var.sa_website_name}" : "${var.sa_website_name}${local.workspace_suffix}"
+  rg_website_name = terraform.workspace == "default" ? var.rg_website_name : var.rg_website_name-local.workspace_suffix
+  sa_website_name = terraform.workspace == "default" ? var.sa_website_name : "${var.sa_website_name}${local.workspace_suffix}"
   web_suffix      = "<h2>${terraform.workspace}</h2>"
 }
